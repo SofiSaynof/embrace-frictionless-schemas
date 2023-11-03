e9a230aa2435b10f9812dd58abf424c1a5ac7272
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-11-03 07:55:46 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,29 @@
+    for (let i = 0; i < 10; i++) {
+
+    data.forEach(item => {
+main();
+    }
+    const data = generateRandomData();
+}
+function generateRandomData() {
+        console.log("Random Number: " + item);
+    return data;
+    });
+function main() {
+}
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = [];
+        print(f"Random Number: {item}")
+def main():
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+import random
+
+    for item in data:
+    main()
+if __name__ == "__main__":
+
+def generate_random_data():
