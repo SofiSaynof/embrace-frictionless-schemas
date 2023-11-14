2df42c01d8f7be6005122f0448abffe3e00035b7
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-11-14 11:50:42 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,17 +1,29 @@
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+
+        for (int i = 0; i < 10; i++) {
+        }
+
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        for (int item : data) {
+        }
+}
     }
-    const data = generateRandomData();
-    data.forEach(item => {
-    });
+            data[i] = (int) (Math.random() * 100) + 1;
+    return data;
+
     const data = [];
+    const data = generateRandomData();
+main();
         data.push(Math.floor(Math.random() * 100) + 1);
     for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+
+    });
         console.log("Random Number: " + item);
-
+    }
 }
-
-main();
-function generateRandomData() {
-    return data;
 }
-
 function main() {
+function generateRandomData() {
